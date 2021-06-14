4616b8e89e00c7692637ef35309de2bd7d075f86
Judith-coder <80777018+Judith-coder@users.noreply.github.com>
2021-06-14 16:29:50 +0200

    Added links to GitHub repository an personal linkedin page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This website was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/judith-charpentier-573b1b155/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Judith Charpentier
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/Judith-coder/shecodes-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
