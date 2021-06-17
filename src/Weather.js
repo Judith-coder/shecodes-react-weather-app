30d43f39362de0fc72c8f433196da3690474d44f
Judith-coder <80777018+Judith-coder@users.noreply.github.com>
2021-06-17 11:06:11 +0200

    Added CSS layout

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
@@ -8,28 +9,41 @@ export default function Weather() {
           <input type="search" placeholder="Enter a city"></input>
           <input type="submit" value="Search"></input>
         </form>
-        <h1>Berlin, DE</h1>
-        <h2>Monday | June, 14 | 17h34</h2>
+        <h1>San Francisco, USA</h1>
+        <h2>Wednesday | June, 16 | 08h58</h2>
         <div className="row">
-          <div className="col-4">
-            <div className="row">Clear</div>
-            <div className="row">27°C</div>
+          <div className="col-5">
+            <div className="row current-weather-description">Clear</div>
+            <div className="row">
+              <div className=" col-4 current-temperature">21</div>
+              <div className=" col-4 unit">
+                <div>
+                  <input type="radio" id="celsius" defaultChecked></input>{" "}
+                  <label for="celsius">°C</label>
+                </div>
+                <div>
+                  <input type="radio" id="fahrenheit"></input>{" "}
+                  <label for="fahrenheit">°F</label>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="col-4">
+          <div className="col-2">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
               alt="weather-icon"
+              id="weather-icon"
             />
           </div>
-          <div className="col-4">
-            <ul>
-              <li>Feels like: 27°C</li>
-              <li>Wind: 10km/h</li>
-              <li>Humidity: 38%</li>
-            </ul>
-          </div>
         </div>
+      </div>
+      <div className="row">
+        <ul>
+          <li>🤔 Feels like: 21°C</li>
+          <li>💨 Wind: 5km/h</li>
+          <li>💧 Humidity: 71%</li>
+        </ul>
       </div>
     </div>
   );
